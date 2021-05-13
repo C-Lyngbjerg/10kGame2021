b15b0bc5268caf4d64dad9c81ca5544600e9a46e
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-05-13 12:18:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+
+app.listen(PORT, (error) => {
+    if(error){
+        console.log('An error has occured: ', error);
+    }else {
+        console.log('Server is now running on port ', Number(PORT));
+    }
+});
