8847a9401f97c71c748b8436db20fb0f830d3fe9
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-05-13 16:44:45 +0200

    initial work on using db queries

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const con = require("./util/connection.js");
+const dotenv = require('dotenv').config();
+
+let resultArr = [];
+
+const queryText = 'SELECT * FROM users';
+
+// const testQ = mysql.query('SELECT * FROM users',(err, results, fields) => {
+//     // console.log(results); // results contains rows returned by server
+//     // console.log(fields); // fields contains extra meta data about results, if available
+//     resultArr = results;
+
+//     /*
+//     resultArr = results.map((result) => {
+//         const resObj = 
+//         {
+//             id: result.id, 
+//             email: result.email, 
+//             password: result.u_password, 
+//             name: result.u_name, 
+//             mmr: result.mmr
+//         };
+//         return resObj
+//     });
+//     */
+//   });
 
 app.get("/", (req, res) => {
-    res.send({greetings: 'hello there', testStatement:'this is indeed a test statement'});
+    con.query(queryText, (err, rows, fields) => {
+        if(!err){
+            res.send(rows);
+        }else {
+            console.log(err);
+        }
+    });
 });
 
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, (error) => {
     if(error){
         console.log('An error has occured: ', error);
