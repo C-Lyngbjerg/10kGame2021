b650e28dc46cc64e93d72fada9540eae81622262
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-05-31 14:41:51 +0200

    tried changing name

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const frontPage = fs.readFileSync(pubDir + '/frontpage/frontpage.html', 'utf-8')
 const loginPage = fs.readFileSync(pubDir + '/login/login.html', 'utf-8');
 const playPage = fs.readFileSync(pubDir + '/play/play.html', 'utf-8');
 const profilepage = fs.readFileSync(pubDir + '/profilepage/profile.html','utf-8');
-const leaderboard = fs.readFileSync(pubDir + '/leaderboard/leaderboard.html','utf-8');
+const leaderboardpage = fs.readFileSync(pubDir + '/leaderboardpage/leaderboard.html','utf-8');
 const rulepage = fs.readFileSync(pubDir + '/rules/rules.html','utf-8');
 const chatPage = fs.readFileSync(pubDir + '/chat/chat.html', 'utf-8');
 
@@ -110,7 +110,7 @@ app.get("/profile", (req,res) => {
 });
 
 app.get("/leaderboard", (req,res) => {
-    res.send(header + leaderboard + footer);
+    res.send(header + leaderboardpage + footer);
 });
 
 app.get("/rules", (req,res) => {
